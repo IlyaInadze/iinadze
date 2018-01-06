03c246d8a736f4ce48061a586eff8411039bc597
IlyaInadze <inadze@mail.ru>
2018-01-06 11:50:29 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
+/**
+* Class Класс Calculate
+*
+* @author Ilya Inadze (inadze@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 
 public class Calculate {
+ /** Метод main для вывода на консоль "Hello World"
+  */
   public static void main(String[] args) {
        System.out.println("Hello world.");
  }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Inadze (pinadze@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
