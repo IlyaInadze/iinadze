43649afbe39f6c4c507138e5d2efe223e0f1801b
IlyaInadze <inadze@mail.ru>
2018-06-15 20:19:36 +0300

    3.4. Расстояние между точками в системе координат

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,21 +7,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Inadze Ilya (inadze@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Inadze Ilya (inadze@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Ilya Inadze";
-    String expect = "Echo, echo, echo : Ilya Inadze"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Ilya Inadze";
+        String expect = "Echo, echo, echo : Ilya Inadze";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
