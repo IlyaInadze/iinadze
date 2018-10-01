ae5ed4433d3c20976832569ce18cee479afd3f5e
IlyaInadze <inadze@mail.ru>
2018-10-01 08:44:24 +0300

    6.4. Слова начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int index = 0; index != value.length; index++) {
+            if (value.equals(data)) {
+                continue;
+            }
+            if (!value.equals(data)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
