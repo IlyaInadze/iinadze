79f45ae76d2595c6854fd7ad90016fbfea826791
IlyaInadze <inadze@mail.ru>
2019-07-06 16:30:19 +0300

    1. Рефакторинг - Расстояние между точками.

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -12,9 +12,9 @@ public class Triangle {
         return (ab + ac + bc) / 2;
     }
     public double area() {
-        double ab = this.a.distanceTo(this.b);
-        double ac = this.a.distanceTo(this.c);
-        double bc = this.b.distanceTo(this.c);
+        double ab = this.a.distance(this.b);
+        double ac = this.a.distance(this.c);
+        double bc = this.b.distance(this.c);
         double p = this.period(ab, ac, bc);
         return (this.exist(ab, ac, bc)) ? Math.sqrt(p * (p - ab) * (p - ac) * (p - bc)) : -1;
     }
